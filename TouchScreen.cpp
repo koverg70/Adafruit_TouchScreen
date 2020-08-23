36e2e1ccea206854ae96dd289412b265e38b44aa
koverg70 <koverg70@gmail.com>
2020-08-23 13:48:32 +0200

    Update TouchScreen.cpp

--- TouchScreen.cpp
+++ TouchScreen.cpp
@@ -1,291 +1,181 @@
 // Touch screen library with X Y and Z (pressure) readings as well
 // as oversampling to avoid 'bouncing'
-// (c) ladyada / adafruit
-// Code under MIT License
+// adapted from (c) ladyada / adafruit
 
 #include "Arduino.h"
-#include "pins_arduino.h"
 
-#ifdef __AVR
-#include <avr/pgmspace.h>
-#elif defined(ESP8266)
-#include <pgmspace.h>
-#endif
 #include "TouchScreen.h"
 
-// increase or decrease the touchscreen oversampling. This is a little different
-// than you make think: 1 is no oversampling, whatever data we get is
-// immediately returned 2 is double-sampling and we only return valid data if
-// both points are the same 3+ uses insert sort to get the median value. We
-// found 2 is precise yet not too slow so we suggest sticking with it!
+#define NUMSAMPLES 3  //.kbv
+#if defined(__STM32F1__) || defined(ESP32)   //Maple core
+#define ADC_ADJUST >>2
+#else
+#define ADC_ADJUST
+#endif
 
-#define NUMSAMPLES 2
+TSPoint::TSPoint(void) {
+    x = y = 0;
+}
 
-TSPoint::TSPoint(void) { x = y = 0; }
-/**
- * @brief Construct a new TSPoint::TSPoint object
- *
- * @param x0 The point's X value
- * @param y0 The point's Y value
- * @param z0 The point's Z value
- */
 TSPoint::TSPoint(int16_t x0, int16_t y0, int16_t z0) {
-  x = x0;
-  y = y0;
-  z = z0;
+    x = x0;
+    y = y0;
+    z = z0;
 }
-/**
- * @brief Check if the current point is **not** equivalent to another point
- *
- * @param p1 The other point being checked for equivalence
- * @return `true` : the two points are equivalent
- * `false`: the two points are **not** equivalent
- */
+
 bool TSPoint::operator==(TSPoint p1) {
-  return ((p1.x == x) && (p1.y == y) && (p1.z == z));
+    return  ((p1.x == x) && (p1.y == y) && (p1.z == z));
 }
-/**
- * @brief Check if the current point is **not** equivalent to another point
- *
- * @param p1 The other point being checked for equivalence
 
- * @return `true` :the two points are **not** equivalent
- * `false`: the two points are equivalent
- */
 bool TSPoint::operator!=(TSPoint p1) {
-  return ((p1.x != x) || (p1.y != y) || (p1.z != z));
+    return  ((p1.x != x) || (p1.y != y) || (p1.z != z));
 }
 
-#if (NUMSAMPLES > 2)
 static void insert_sort(int array[], uint8_t size) {
-  uint8_t j;
-  int save;
+    uint8_t j;
+    int save;
 
-  for (int i = 1; i < size; i++) {
-    save = array[i];
-    for (j = i; j >= 1 && save < array[j - 1]; j--)
-      array[j] = array[j - 1];
-    array[j] = save;
-  }
+    for (int i = 1; i < size; i++) {
+        save = array[i];
+        for (j = i; j >= 1 && save < array[j - 1]; j--)
+            array[j] = array[j - 1];
+        array[j] = save;
+    }
 }
-#endif
-/**
- * @brief Measure the X, Y, and pressure and return a TSPoint with the
- * measurements
- *
- * @return TSPoint The measured X, Y, and Z/pressure values
- */
+
 TSPoint TouchScreen::getPoint(void) {
-  int x, y, z;
-  int samples[NUMSAMPLES];
-  uint8_t i, valid;
+    int x, y, z;
+    int samples[NUMSAMPLES];
+    uint8_t i, valid;
 
-  valid = 1;
+    valid = 1;
 
-  pinMode(_yp, INPUT);
-  pinMode(_ym, INPUT);
-  pinMode(_xp, OUTPUT);
-  pinMode(_xm, OUTPUT);
+    pinMode(_yp, INPUT);
+    pinMode(_ym, INPUT);
 
-#if defined(USE_FAST_PINIO)
-  *xp_port |= xp_pin;
-  *xm_port &= ~xm_pin;
-#else
-  digitalWrite(_xp, HIGH);
-  digitalWrite(_xm, LOW);
-#endif
+    digitalWrite(_yp, LOW);
+    digitalWrite(_ym, LOW);
 
-#ifdef __arm__
-  delayMicroseconds(20); // Fast ARM chips need to allow voltages to settle
-#endif
+    pinMode(_xp, OUTPUT);
+    pinMode(_xm, OUTPUT);
+    digitalWrite(_xp, HIGH);
+    digitalWrite(_xm, LOW);
 
-  for (i = 0; i < NUMSAMPLES; i++) {
-    samples[i] = analogRead(_yp);
-  }
+    for (i = 0; i < NUMSAMPLES; i++) {
+        samples[i] = analogRead(_yp) ADC_ADJUST;
+    }
+    insert_sort(samples, NUMSAMPLES);
+    x = (1023 - samples[NUMSAMPLES / 2]); //choose median
 
-#if NUMSAMPLES > 2
-  insert_sort(samples, NUMSAMPLES);
-#endif
-#if NUMSAMPLES == 2
-  // Allow small amount of measurement noise, because capacitive
-  // coupling to a TFT display's signals can induce some noise.
-  if (samples[0] - samples[1] < -4 || samples[0] - samples[1] > 4) {
-    valid = 0;
-  } else {
-    samples[1] = (samples[0] + samples[1]) >> 1; // average 2 samples
-  }
-#endif
+    pinMode(_xp, INPUT);
+    pinMode(_xm, INPUT);
+    digitalWrite(_xp, LOW);
+    digitalWrite(_xm, LOW);     //.kbv for Due
 
-  x = (1023 - samples[NUMSAMPLES / 2]);
+    pinMode(_yp, OUTPUT);
+    digitalWrite(_yp, HIGH);
+    pinMode(_ym, OUTPUT);
+    digitalWrite(_ym, LOW);     //.kbv for Due
 
-  pinMode(_xp, INPUT);
-  pinMode(_xm, INPUT);
-  pinMode(_yp, OUTPUT);
-  pinMode(_ym, OUTPUT);
+    for (i = 0; i < NUMSAMPLES; i++) {
+        samples[i] = analogRead(_xm) ADC_ADJUST;
+    }
 
-#if defined(USE_FAST_PINIO)
-  *ym_port &= ~ym_pin;
-  *yp_port |= yp_pin;
-#else
-  digitalWrite(_ym, LOW);
-  digitalWrite(_yp, HIGH);
-#endif
+    insert_sort(samples, NUMSAMPLES);
 
-#ifdef __arm__
-  delayMicroseconds(20); // Fast ARM chips need to allow voltages to settle
-#endif
+    y = (1023 - samples[NUMSAMPLES / 2]);
 
-  for (i = 0; i < NUMSAMPLES; i++) {
-    samples[i] = analogRead(_xm);
-  }
+    // Set X+ to ground
+    pinMode(_xp, OUTPUT);
+    digitalWrite(_xp, LOW);
 
-#if NUMSAMPLES > 2
-  insert_sort(samples, NUMSAMPLES);
-#endif
-#if NUMSAMPLES == 2
-  // Allow small amount of measurement noise, because capacitive
-  // coupling to a TFT display's signals can induce some noise.
-  if (samples[0] - samples[1] < -4 || samples[0] - samples[1] > 4) {
-    valid = 0;
-  } else {
-    samples[1] = (samples[0] + samples[1]) >> 1; // average 2 samples
-  }
-#endif
+    // Set Y- to VCC
+    pinMode(_ym, OUTPUT);     //.kbv
+    digitalWrite(_ym, HIGH);
 
-  y = (1023 - samples[NUMSAMPLES / 2]);
+    // Hi-Z X- and Y+
+    digitalWrite(_xm, LOW);   //.kbv
+    pinMode(_xm, INPUT);      //.kbv
+    digitalWrite(_yp, LOW);
+    pinMode(_yp, INPUT);
 
-  // Set X+ to ground
-  // Set Y- to VCC
-  // Hi-Z X- and Y+
-  pinMode(_xp, OUTPUT);
-  pinMode(_yp, INPUT);
+    int z1 = analogRead(_xm) ADC_ADJUST;
+    int z2 = analogRead(_yp) ADC_ADJUST;
 
-#if defined(USE_FAST_PINIO)
-  *xp_port &= ~xp_pin;
-  *ym_port |= ym_pin;
-#else
-  digitalWrite(_xp, LOW);
-  digitalWrite(_ym, HIGH);
-#endif
-
-  int z1 = analogRead(_xm);
-  int z2 = analogRead(_yp);
-
-  if (_rxplate != 0) {
-    // now read the x
-    float rtouch;
-    rtouch = z2;
-    rtouch /= z1;
-    rtouch -= 1;
-    rtouch *= x;
-    rtouch *= _rxplate;
-    rtouch /= 1024;
-
-    z = rtouch;
-  } else {
     z = (1023 - (z2 - z1));
-  }
 
-  if (!valid) {
-    z = 0;
-  }
-
-  return TSPoint(x, y, z);
+    return TSPoint(x, y, z);  //XM, YP still in ANALOG mode
 }
+
+TouchScreen::TouchScreen(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym) {
+    _yp = yp;
+    _xm = xm;
+    _ym = ym;
+    _xp = xp;
+    _rxplate = 0;
+    pressureThreshhold = 10;
+}
+
 
 TouchScreen::TouchScreen(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym,
-                         uint16_t rxplate = 0) {
-  _yp = yp;
-  _xm = xm;
-  _ym = ym;
-  _xp = xp;
-  _rxplate = rxplate;
+                         uint16_t rxplate) {
+    _yp = yp;
+    _xm = xm;
+    _ym = ym;
+    _xp = xp;
+    _rxplate = rxplate;
 
-#if defined(USE_FAST_PINIO)
-  xp_port = portOutputRegister(digitalPinToPort(_xp));
-  yp_port = portOutputRegister(digitalPinToPort(_yp));
-  xm_port = portOutputRegister(digitalPinToPort(_xm));
-  ym_port = portOutputRegister(digitalPinToPort(_ym));
-
-  xp_pin = digitalPinToBitMask(_xp);
-  yp_pin = digitalPinToBitMask(_yp);
-  xm_pin = digitalPinToBitMask(_xm);
-  ym_pin = digitalPinToBitMask(_ym);
-#endif
-
-  pressureThreshhold = 10;
+    pressureThreshhold = 10;
 }
-/**
- * @brief Read the touch event's X value
- *
- * @return int the X measurement
- */
+
 int TouchScreen::readTouchX(void) {
-  pinMode(_yp, INPUT);
-  pinMode(_ym, INPUT);
-  digitalWrite(_yp, LOW);
-  digitalWrite(_ym, LOW);
+    pinMode(_yp, INPUT);
+    pinMode(_ym, INPUT);
+    digitalWrite(_yp, LOW);
+    digitalWrite(_ym, LOW);
 
-  pinMode(_xp, OUTPUT);
-  digitalWrite(_xp, HIGH);
-  pinMode(_xm, OUTPUT);
-  digitalWrite(_xm, LOW);
+    pinMode(_xp, OUTPUT);
+    digitalWrite(_xp, HIGH);
+    pinMode(_xm, OUTPUT);
+    digitalWrite(_xm, LOW);
 
-  return (1023 - analogRead(_yp));
+    return (1023 - (analogRead(_yp)) ADC_ADJUST);
 }
-/**
- * @brief Read the touch event's Y value
- *
- * @return int the Y measurement
- */
+
+
 int TouchScreen::readTouchY(void) {
-  pinMode(_xp, INPUT);
-  pinMode(_xm, INPUT);
-  digitalWrite(_xp, LOW);
-  digitalWrite(_xm, LOW);
+    pinMode(_xp, INPUT);
+    pinMode(_xm, INPUT);
+    digitalWrite(_xp, LOW);
+    digitalWrite(_xm, LOW);
 
-  pinMode(_yp, OUTPUT);
-  digitalWrite(_yp, HIGH);
-  pinMode(_ym, OUTPUT);
-  digitalWrite(_ym, LOW);
+    pinMode(_yp, OUTPUT);
+    digitalWrite(_yp, HIGH);
+    pinMode(_ym, OUTPUT);
+    digitalWrite(_ym, LOW);
 
-  return (1023 - analogRead(_xm));
+    return (1023 - (analogRead(_xm)) ADC_ADJUST);
 }
-/**
- * @brief Read the touch event's Z/pressure value
- *
- * @return int the Z measurement
- */
+
+
 uint16_t TouchScreen::pressure(void) {
-  // Set X+ to ground
-  pinMode(_xp, OUTPUT);
-  digitalWrite(_xp, LOW);
+    // Set X+ to ground
+    pinMode(_xp, OUTPUT);
+    digitalWrite(_xp, LOW);
 
-  // Set Y- to VCC
-  pinMode(_ym, OUTPUT);
-  digitalWrite(_ym, HIGH);
+    // Set Y- to VCC
+    pinMode(_ym, OUTPUT);
+    digitalWrite(_ym, HIGH);
 
-  // Hi-Z X- and Y+
-  digitalWrite(_xm, LOW);
-  pinMode(_xm, INPUT);
-  digitalWrite(_yp, LOW);
-  pinMode(_yp, INPUT);
+    // Hi-Z X- and Y+
+    digitalWrite(_xm, LOW);
+    pinMode(_xm, INPUT);
+    digitalWrite(_yp, LOW);
+    pinMode(_yp, INPUT);
 
-  int z1 = analogRead(_xm);
-  int z2 = analogRead(_yp);
+    int z1 = analogRead(_xm) ADC_ADJUST;
+    int z2 = analogRead(_yp) ADC_ADJUST;
 
-  if (_rxplate != 0) {
-    // now read the x
-    float rtouch;
-    rtouch = z2;
-    rtouch /= z1;
-    rtouch -= 1;
-    rtouch *= readTouchX();
-    rtouch *= _rxplate;
-    rtouch /= 1024;
-
-    return rtouch;
-  } else {
     return (1023 - (z2 - z1));
-  }
 }
+
