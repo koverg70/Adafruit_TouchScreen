6c6f0e4e63c48fb32240a850d58bca86e4578ef2
koverg70 <koverg70@gmail.com>
2020-08-23 13:46:50 +0200

    Update TouchScreen.h

--- TouchScreen.h
+++ TouchScreen.h
@@ -3,75 +3,41 @@
 // (c) ladyada / adafruit
 // Code under MIT License
 
-#ifndef _ADAFRUIT_TOUCHSCREEN_H_
-#define _ADAFRUIT_TOUCHSCREEN_H_
+#ifndef _TOUCHSCREEN_KBV_H_
+#define _TOUCHSCREEN_KBV_H_
 #include <stdint.h>
 
-#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega32U4__) ||              \
-    defined(TEENSYDUINO) || defined(__AVR_ATmega2560__) ||                     \
-    defined(__AVR_ATmega4809__)
-typedef volatile uint8_t RwReg;
-#elif defined(ARDUINO_STM32_FEATHER)
-typedef volatile uint32 RwReg;
-#elif defined(NRF52_SERIES) || defined(ESP32) || defined(ESP8266) ||           \
-    defined(ARDUINO_ARCH_STM32)
-typedef volatile uint32_t RwReg;
-#else
-typedef volatile uint32_t RwReg;
-#endif
+class TSPoint_kbv {
+    public:
+        TSPoint_kbv(void);
+        TSPoint_kbv(int16_t x, int16_t y, int16_t z);
 
-#if defined(__AVR__) || defined(TEENSYDUINO) || defined(ARDUINO_ARCH_SAMD)
-#define USE_FAST_PINIO
-#endif
+        bool operator==(TSPoint_kbv);
+        bool operator!=(TSPoint_kbv);
 
-/** Object that encapsulates the X,Y, and Z/pressure measurements for a touch
- * event. */
-class TSPoint {
-public:
-  TSPoint(void);
-  TSPoint(int16_t x, int16_t y, int16_t z);
-
-  bool operator==(TSPoint);
-  bool operator!=(TSPoint);
-
-  int16_t x, ///< state variable for the x value
-      y,     ///< state variable for the y value
-      z;     ///< state variable for the z value
+        int16_t x, y, z;
 };
-/** Object that controls and keeps state for a touch screen. */
 
-class TouchScreen {
-public:
-  /**
-   * @brief Construct a new Touch Screen object
-   *
-   * @param xp X+ pin. Must be an analog pin
-   * @param yp Y+ pin. Must be an analog pin
-   * @param xm X- pin. Can be a digital pin
-   * @param ym Y- pin. Can be a digital pin
-   * @param rx The resistance in ohms between X+ and X- to calibrate pressure
-   * sensing
-   */
-  TouchScreen(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym, uint16_t rx);
+class TouchScreen_kbv {
+    public:
+        TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym);
+        TouchScreen_kbv(uint8_t xp, uint8_t yp, uint8_t xm, uint8_t ym, uint16_t rx);
 
-  /**
-   * @brief **NOT IMPLEMENTED** Test if the screen has been touched
-   *
-   * @return true : touch detected false: no touch detected
-   */
-  bool isTouching(void);
-  uint16_t pressure(void);
-  int readTouchY();
-  int readTouchX();
-  TSPoint getPoint();
-  int16_t pressureThreshhold; ///< Pressure threshold for `isTouching`
+//        bool isTouching(void);
+        uint16_t pressure(void);
+        int readTouchY();
+        int readTouchX();
+        TSPoint_kbv getPoint();
+        int16_t pressureThreshhold;
 
-private:
-  uint8_t _yp, _ym, _xm, _xp;
-  uint16_t _rxplate;
-
-  volatile RwReg *xp_port, *yp_port, *xm_port, *ym_port;
-  RwReg xp_pin, xm_pin, yp_pin, ym_pin;
+    private:
+        uint8_t _yp, _ym, _xm, _xp;
+        uint16_t _rxplate;
 };
 
 #endif
+
+
+//Added by Sloeber 
+#pragma once
+
